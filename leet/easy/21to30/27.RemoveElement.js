5f464effe1a1559ae1a6c8354268e35a71ef5ba0
mikyYun <mkyun2714@gmail.com>
2022-07-03 22:54:35 -0700

    27.RemoveELement

---
+++ leet/easy/21to30/27.RemoveElement.js
@@ -0,0 +1,15 @@
+const removeElement = (nums, val) => {
+  // failed approach
+  // return nums = nums.filter(num => num !== val);
+
+  for (let i = 0; i < nums.length; i ++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1)
+      i --
+    }
+  }
+};
+const nums = [0, 1, 2, 2, 3, 0, 4, 2];
+const val = 2;
+
+console.log(removeElement(nums, val));
