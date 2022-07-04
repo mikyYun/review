af4706a171e15dd5226155c7b428ab50beea7aa2
mikyYun <mkyun2714@gmail.com>
2022-07-04 15:09:58 -0700

    35.Search Insert Position

---
+++ leet/easy/31to40/35.SearchInsertPosition.js
@@ -0,0 +1,8 @@
+const searchInsert = (nums, target) => {
+  nums = nums.filter(num => num <= target)
+  console.log(nums)
+  return nums.includes(target) ? nums.length - 1 : nums.length
+};
+const nums = [1, 3, 5, 6];
+const target = 5;
+console.log(searchInsert(nums, target))
