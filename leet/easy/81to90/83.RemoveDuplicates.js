7453745be239d1fb0ff11d871163c2bf280fc580
mikyYun <mkyun2714@gmail.com>
2022-07-07 18:07:36 -0700

    83.RemoveDuplicates

---
+++ leet/easy/81to90/83.RemoveDuplicates.js
@@ -0,0 +1,23 @@
+function ListNode(val, next) {
+  this.val = (val === undefined ? 0 : val);
+  this.next = (next === undefined ? null : next);
+}
+
+const deleteDuplicates = (head) => {
+  // if head.val && head.next => run
+  // else return head (head.val = exist, head.val = undefined)
+  // edge
+  if(head === null || head.next ===null) return head;
+  if (head.next !== null) {
+    if (head.val === head.next.val) {
+      head = deleteDuplicates(head.next);
+    } else {
+      head.next = deleteDuplicates(head.next);
+    }
+  }
+
+  return head;
+
+};
+const head = [1, 1, 2, 3, 4];
+console.log(deleteDuplicates(head));
